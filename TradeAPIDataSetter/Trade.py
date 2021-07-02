208e2b63ad7f4c7ab3d44e68071f874d29582080
Andy Cruse <75581502+Andy-Cruse@users.noreply.github.com>
2021-07-02 00:07:54 -0400

    Update Trade.py

--- TradeAPIDataSetter/Trade.py
+++ TradeAPIDataSetter/Trade.py
@@ -5,22 +5,23 @@ from TradeAPIDataSetter.secrets import Secrets
 import matplotlib.pyplot as plt
 import mysql.connector
 
+# IN NEED OF SQL TABLE DUPLICATE REMOVER
 class Trade:
     def __init__(self, symbol, interval):
         sec = Secrets()  # API key stored in secrets.py
-        self.token = sec.api_token
-        self.user = sec.username
+        self.token = sec.api_token # Need to get your own API key. The one I used, AlphaVantage, was free
+        self.user = sec.username 
         self.passwd = sec.password
         self.symbol = symbol
         self.interval = interval
-        self.data = Trade.connect_to_api(self)
+        self.data = Trade.connect_to_api(self) # returns data as dictionary
         self.time_series = self.data[f'Time Series ({self.interval}min)']
         self.meta_data = self.data['Meta Data']
         self.data = pd.DataFrame.from_dict(self.data)
         self.data = self.data.transpose().sort_index()
         self.time_series = pd.DataFrame.from_dict(self.time_series)
         self.time_series = self.time_series.transpose().sort_index()
-        # self.meta_data = pd.DataFrame.from_dict(self.meta_data)
+        # self.meta_data = pd.DataFrame.from_dict(self.meta_data) # Had issues running this line. The data is not needed for anything, but still...
         # self.meta_data = self.meta_data.transpose().sort_index()
 
     # Returns full JSON from API call
@@ -30,11 +31,13 @@ class Trade:
         data = r.json()
         return data
 
+    # Displays the time series for open, high, low, close, and volume
     def disp_time_series(self):
         df = self.time_series
         print(df)
         print(df['1. open'])
         fig, ax = plt.subplots(5)
+        # There is likely a better way to do this
         ax[0].plot(df.index, df['1. open'])
         ax[0].legend(['Open'])
         ax[0].set_xticklabels([])
@@ -58,6 +61,25 @@ class Trade:
         fig.suptitle(f'{self.symbol} Stock {df.index[0]} to {df.index[len(df)-1]}')
         plt.show()
 
+    # Creates a MySQL table
+    # While commenting, I notice that it would be a good idea to set the database name to a variable. Will update
+    def create_mysql_table(self):
+        db = mysql.connector.connect(host='localhost',
+                                     user=self.user,
+                                     passwd=self.passwd)
+        mycursor = db.cursor()
+        mycursor.execute('USE python_stock')
+        mycursor.execute(f'CREATE TABLE {self.symbol}_{self.interval}min '
+                         f'(date VARCHAR(100), '
+                         f'open VARCHAR(10), '
+                         f'high VARCHAR(10), '
+                         f'low VARCHAR(10), '
+                         f'close VARCHAR(10), '
+                         f'volume VARCHAR(10))')
+        db.commit()
+
+    
+    # Adds data for each minute interval specified earlier to a MySQL table
     def add_to_mysql(self):
         db = mysql.connector.connect(host='localhost',
                                      user=self.user,
@@ -74,23 +96,9 @@ class Trade:
             sql = 'INSERT INTO '+self.symbol+'_'+self.interval+'min VALUES ('+index+', '+ope+', '+high+', '+low+', '+close+', '+volume+')'
             mycursor.execute(sql)
         db.commit()
-
-    def create_mysql_table(self):
-        db = mysql.connector.connect(host='localhost',
-                                     user=self.user,
-                                     passwd=self.passwd)
-        mycursor = db.cursor()
-        mycursor.execute('USE python_stock')
-        mycursor.execute(f'CREATE TABLE {self.symbol}_{self.interval}min '
-                         f'(date VARCHAR(100), '
-                         f'open VARCHAR(10), '
-                         f'high VARCHAR(10), '
-                         f'low VARCHAR(10), '
-                         f'close VARCHAR(10), '
-                         f'volume VARCHAR(10))')
-        db.commit()
-
-    # Old method I used previously to make excel sheets
+        
+        
+    # Old method I used previously to make excel sheets. Excel is expensive.
     # Not needed, but I don't wanna toss it
     # def write_to_excel(self, dataframe):
     #     writer = pd.ExcelWriter('recommended trades.xlsx', engine='xlsxwriter')
